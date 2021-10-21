676054ad757dfae871b8910c873d3d0ad9db3197
rollwagen <rollwagen@gmail.com>
2021-10-21 15:37:02 +0200

    Add delta tool install

--- ubuntu.sh
+++ ubuntu.sh
@@ -9,3 +9,7 @@ https://raw.githubusercontent.com/jnurmine/Zenburn/master/colors/zenburn.vim
 
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# rust and delta (diff viewer)
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+~/.cargo/bin/cargo install git-delta
