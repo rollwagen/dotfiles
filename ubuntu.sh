3da49958174ffef7e41e42f4d4da7b952edf9dc7
rollwagen <rollwagen@gmail.com>
2021-10-21 15:28:57 +0200

    Inital commit

---
+++ ubuntu.sh
@@ -0,0 +1,8 @@
+
+# vim / vimplug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# vim / zenburn theme
+curl -sLo ~/.vim/colors/zenburn.vim --create-dirs \
+https://raw.githubusercontent.com/jnurmine/Zenburn/master/colors/zenburn.vim
