7eaf2b9cb038b4fe71c3b84ca99b57294657ea5b
rollwagen <rollwagen@gmail.com>
2022-08-20 18:46:46 +0200

    Add shell script to install go tools

---
+++ go.sh
@@ -0,0 +1,6 @@
+go install github.com/goreleaser/goreleaser@latest
+go install github.com/rakyll/gotest@latest
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+go install golang.org/x/tools/cmd/stringer@latest
+go install github.com/mgechev/revive@latest
+go install golang.org/x/tools/cmd/goimports@latest
